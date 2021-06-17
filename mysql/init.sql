8e480397561c800df388faffff5f21403ecdd49b
Steven Gee <ioexcept@gmail.com>
2021-06-17 13:37:59 +0000

    backup push

--- mysql/init.sql
+++ mysql/init.sql
@@ -5,6 +5,7 @@ USE scorecard;
 CREATE TABLE rawdata (
        	pluginid varchar(8) NOT NULL,
 	host varchar(32) NOT NULL,
+	riskid int NOT NULL,
 	rptdate DATE NOT NULL
 );
 
@@ -12,6 +13,21 @@ CREATE TABLE plugin (
        	pluginid varchar(8) NOT NULL,
 	vulname varchar(64) NOT NULL,
 	risk varchar(12) NOT NULL,
-	riskid int,
+	riskid int NOT NULL,
 	PRIMARY KEY (pluginid)
 );
+
+CREATE TABLE risk(
+	riskid int NOT NULL,
+	risk VARCHAR(12) NOT NULL,
+	PRIMARY KEY (riskid)
+);
+
+insert into risk (riskid,risk) values
+	(0,'Critical'),
+	(1,'High'),
+	(2,'Medium'),
+	(3,'Low'),
+	(-1,'None'),
+	(-99,'Undefined');
+
--- scorecard.py
+++ scorecard.py
@@ -5,16 +5,14 @@ import mysql.connector
 from mysql.connector import connect, Error
 
 
-
-
-def XXXmainXXX():
-    cntr=0
-    while cntr < 1000:
-        if (cntr % 100) == 0:
-            print("modulous found: %s" , (cntr))
-        cntr += 1
-
-
+def fetchrefid(risk):
+    result = -99;
+    if risk == 'Critical': result = 0
+    elif risk == 'High': result = 1
+    elif risk == 'Medium': result = 2
+    elif risk == 'Low': result = 3
+    elif risk == 'None': result = -1
+    return result
 
 
 def main():
@@ -34,12 +32,12 @@ def main():
            database='scorecard')
            print(cnx)
            mycursor = cnx.cursor()
-           sql = "insert into rawdata(pluginid,host,rptdate) values (%s, %s, %s)"
-           #sql = "insert into rawdata(pluginid,host,vulname,rptdate) values (%s, %s, %s, %s)"
+           sql = "insert into rawdata(pluginid,host,riskid,rptdate) values (%s, %s, %s)"
+           #sql = "insert into rawdata(pluginid,host,vulname,riskid,rptdate) values (%s, %s, %s, %s, %s)"
            count = 0
            for lines in csvFile:
                if count > 0:
-                   record = (lines[0],lines[4],dt)
+                   record = (lines[0],lines[4],fetchrefid(lines[3]),dt)
                    #print(record)
                    #print(lines[0] + ' ' + lines[4] + ' ' + dt)
                    mycursor.execute(sql, record)
@@ -58,36 +56,5 @@ def main():
            print(e)
        
 
-def loadXRef():
-   # opening the CSV file
-   with open('../sc.data/data.csv.old', mode ='r')as file:
-
-   # reading the CSV file
-       csvFile = csv.reader(file)
-   
-   # displaying the contents of the CSV file
-       try:
-           cnx = mysql.connector.connect(user='scorecard', 
-           password='scorecard',
-           host='127.0.0.1',
-           database='scorecard')
-           print(cnx)
-           mycursor = cnx.cursor()
-           xref = "insert ignore into plugin (pluginid,name) values(%s,%s)"
-           count = 0
-           for lines in csvFile:
-               if count > 0:
-                   vulname = lines[7]
-                   record = (lines[0],vulname[:64])
-                   print(record)
-                   mycursor.execute(xref, record)
-                   cnx.commit()
-               count+=1
-
-           cnx.commit()
-           print("Total xref records loaded: " , count)
-       except Error as e:
-           print(e)
-       
 if __name__ == "__main__":
     main()
