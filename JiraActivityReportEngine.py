4e5a1d194617c1c89baeb803b6f61ccf9696a37c
ioexcept <ioexcept@yahoo.com>
2024-10-01 17:02:04 -0500

    more changes to the reporting. It's working, but until we have the email code installed, you wont know how it looks aesthetically
    TODO: Still need to add email feature

--- JiraActivityReportEngine.py
+++ JiraActivityReportEngine.py
@@ -38,12 +38,12 @@ class ReportEngine:
         self.jql.append("status = Validation AND statuscategorychangeddate >= startOfDay(-1) and statusCategoryChangedDate <= endOfDay(-1)")
         self.jql.append("status = Done AND statuscategorychangeddate >= startOfDay(-1) and statusCategoryChangedDate <= endOfDay(-1)")
         self.jql.append("createdDate >= startOfDay(-1) and createdDate <= endOfDay(-1)")
+
         self.jql.append("status = Validation AND statuscategorychangeddate >= startOfWeek(-1) and statusCategoryChangedDate <= endOfWeek(-1)")
         self.jql.append("status = Done AND statuscategorychangeddate >= startOfWeek(-1) and statusCategoryChangedDate <= endOfWeek(-1)")
         self.jql.append("createdDate >= startOfWeek(-1) and createdDate <= endOfWeek(-1)")
-        self.jql.append("status = Validation AND statuscategorychangeddate >= startOfMonth(-1) and statusCategoryChangedDate <= endOfMonth(-1)")
-        self.jql.append("status = Done AND statuscategorychangeddate >= startOfMonth(-1) and statusCategoryChangedDate <= endOfMonth(-1)")
-        self.jql.append("createdDate >= startOfMonth(-1) and createdDate <= endOfMonth(-1)")
+
+
         self.jqltitle = []
         self.jqltitle.append(f"Validated Yesterday [{formatted_yesterday}]")
         self.jqltitle.append("Done Yesterday")
@@ -51,9 +51,6 @@ class ReportEngine:
         self.jqltitle.append("Validated Last Week")
         self.jqltitle.append("Done Last Week")
         self.jqltitle.append("Created Last Week")
-        self.jqltitle.append("Validated Last Month")
-        self.jqltitle.append("Done Last Month")
-        self.jqltitle.append("Created Last Month")
 
         self.run_daily_report = True
         self.run_weekly_report = today.weekday() == 0
@@ -62,8 +59,24 @@ class ReportEngine:
         self.email_list = []
 
     def determineReportParameters(self):
+
+        monthlyjql = {
+            "validation": {
+                "title": "Validated Last Month",
+                "sql": "status = Validation AND statuscategorychangeddate >= startOfMonth(-1) and statusCategoryChangedDate <= endOfMonth(-1) order by priority desc"
+            },
+            "done": {
+                "title": "Done Last Month",
+                "sql": "status = Done AND statuscategorychangeddate >= startOfMonth(-1) and statusCategoryChangedDate <= endOfMonth(-1) order by priority desc"
+            },
+            "open": {
+                "title": "Created Last Month",
+                "sql": "createdDate >= startOfMonth(-1) and createdDate <= endOfMonth(-1) order by priority desc"
+            }
+        }
+
         try:
-            print('determining date to run')
+            #print('determining date to run')
 
             if self.run_daily_report:
                 print(f'Run Daily Report (RUN_DAILY_REPORT {self.run_daily_report})')
@@ -73,31 +86,50 @@ class ReportEngine:
 
             if self.run_monthly_report:
                 print(f'Run Monthly Report (RUN_MONTHLY_REPORT {self.run_monthly_report})')
-
-            data = [
-                "John,Doe,30,john@example.com",
-                "Jane,Smith,25,jane@example.com",
-                "Mike,Johnson,40,mike@example.com"
-            ]
-
-            headers = ["First Name", "Last Name", "Age", "Email"]
-            result = f"{headers[0]:<12} {headers[1]:<12} {headers[2]:<5} {headers[3]:<20}\n"
-            result += "-" * 50 + "\n"
-
-            # Format each row
-            for row in data:
-                row_data = row.split(',')
-                result += f"{row_data[0]:<12} {row_data[1]:<12} {row_data[2]:<5} {row_data[3]:<20}\n"
-
-            print (result)
-#            parsed_data = [row.split(',') for row in data]
-#            df = pd.DataFrame(parsed_data, columns=["First Name", "Last Name", "Age", "Email"])
-#            df.to_excel("output.xlsx", index=False)
-            print("Data has been written to output.xlsx")
+                self.runReport(monthlyjql)
 
         except Exception as e:
-            print('blob')
+            self.logger.error("An error occurred in determining runtime ...")
+            self.logger.error(e)
 
+    def runReport(self, datadict):
+        headers = ["Issue", "Level", "Created", "Description"]
+        result = f"{headers[0]:<12} {headers[1]:<10} {headers[2]:<12} {headers[3]:<256}\n"
+        result += "-" * 50 + "\n"
+
+        try:
+            for query_name, query_data in datadict.items():
+                result += f"{query_data['title']:<30}\n"
+                url = f"{jira_url}/rest/api/3/search"
+
+                params = {
+                    "jql": query_data['sql'],
+                    "maxResults": 250,  # Optional: Change if you want to limit the results
+                }
+                auth = HTTPBasicAuth(email, api_token)
+                headers = {
+                    "Accept": "application/json"
+                }
+                response = requests.get(url, headers=headers, auth=auth, params=params)
+                print(f"Reporting criteria: {query_data['title']}")
+                if response.status_code == 200:
+                    #print('RESPONSE_CODE_200')
+                    data = response.json()
+                    if data['total'] == 0:
+                        print('No issues found for given period ... ')
+                    else:
+                        for issue in data['issues']:
+                            date_object = datetime.strptime(f"{issue['fields']['created']}", '%Y-%m-%dT%H:%M:%S.%f%z')
+                            result += f"{issue['key']:<12} {issue['fields']['priority']['name']:<10} {date_object.strftime('%m/%d/%y'):<12} {issue['fields']['summary']:<256}\n"
+                else:
+                    self.logger.error(f"Error fetching issue: {response.status_code} - {response.text}")
+            print('-----------------------------------------------------------------------------')
+            print(result)
+
+
+        except Exception as e:
+            self.logger.error("An error occurred in data processing ...")
+            self.logger.error(e)
 
     def runWeeklyReport(self):
         print('*****WEEKLY*****')
@@ -107,8 +139,6 @@ class ReportEngine:
 
     def runDailyReport(self):
         print('*****DAILY*****')
-        if 1==1:
-            exit(-1)
 
         try:
             #jql_query = "status = Done AND statuscategorychangeddate >= startOfDay(-1) and statusCategoryChangedDate <= endOfDay(-1)"
@@ -246,17 +276,17 @@ class ReportEngine:
             print("Exactly one of the argument -a, -b, or -c must be provided.")
             exit(-1)
 
-        if only_key == 0:
+        if only_key == 1:
             print('ONLY running DAILY report')
             self.run_daily_report = True
             self.run_weekly_report = False
             self.run_monthly_report = False
-        elif only_key == 1:
+        elif only_key == 2:
             print('ONLY running WEEKLY report')
             self.run_daily_report = False
             self.run_weekly_report = True
             self.run_monthly_report = False
-        elif only_key == 2:
+        elif only_key == 3:
             print('ONLY running MONTHLY report')
             self.run_daily_report = False
             self.run_weekly_report = False
