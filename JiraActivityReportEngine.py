443172fa7b22ccd18a7ef07daf1a21d849599d10
ioexcept <ioexcept@yahoo.com>
2024-10-02 17:24:31 -0500

    Excel test class created and working, it needs to beimplemented into the overall project
    TODO: Still need to add email feature

--- JiraActivityReportEngine.py
+++ JiraActivityReportEngine.py
@@ -34,23 +34,6 @@ class ReportEngine:
         self.logger.addHandler(file_handler)
         self.logger.addHandler(console_handler)
         self.logger.info('******* Initializing JiraActivityReportEngine!!! *********')
-        self.jql = []
-        self.jql.append("status = Validation AND statuscategorychangeddate >= startOfDay(-1) and statusCategoryChangedDate <= endOfDay(-1)")
-        self.jql.append("status = Done AND statuscategorychangeddate >= startOfDay(-1) and statusCategoryChangedDate <= endOfDay(-1)")
-        self.jql.append("createdDate >= startOfDay(-1) and createdDate <= endOfDay(-1)")
-
-        self.jql.append("status = Validation AND statuscategorychangeddate >= startOfWeek(-1) and statusCategoryChangedDate <= endOfWeek(-1)")
-        self.jql.append("status = Done AND statuscategorychangeddate >= startOfWeek(-1) and statusCategoryChangedDate <= endOfWeek(-1)")
-        self.jql.append("createdDate >= startOfWeek(-1) and createdDate <= endOfWeek(-1)")
-
-
-        self.jqltitle = []
-        self.jqltitle.append(f"Validated Yesterday [{formatted_yesterday}]")
-        self.jqltitle.append("Done Yesterday")
-        self.jqltitle.append("Created Yesterday")
-        self.jqltitle.append("Validated Last Week")
-        self.jqltitle.append("Done Last Week")
-        self.jqltitle.append("Created Last Week")
 
         self.run_daily_report = True
         self.run_weekly_report = today.weekday() == 0
@@ -167,100 +150,6 @@ class ReportEngine:
             self.logger.error("An error occurred in data processing ...")
             self.logger.error(e)
 
-    def runWeeklyReport(self):
-        print('*****WEEKLY*****')
-    def runMonthlyReport(self):
-        print('*****MONTHLY*****')
-
-
-    def runDailyReport(self):
-        print('*****DAILY*****')
-
-        try:
-            #jql_query = "status = Done AND statuscategorychangeddate >= startOfDay(-1) and statusCategoryChangedDate <= endOfDay(-1)"
-            url = f"{jira_url}/rest/api/3/search"
-            for i in range(9):
-
-                params = {
-                    "jql": self.jql[i],
-                    "maxResults": 100,  # Optional: Change if you want to limit the results
-                }
-                auth = HTTPBasicAuth(email, api_token)
-                headers = {
-                    "Accept": "application/json"
-                }
-                response = requests.get(url, headers=headers, auth=auth, params=params)
-                print(f"Reporting criteria: {self.jqltitle[i]}")
-
-                #The URL for the issue is https://wmmc.atlassian.net/browse/{issue['key']}
-
-                if response.status_code == 200:
-                    #print('RESPONSE_CODE_200')
-                    data = response.json()
-                    if data['total'] == 0:
-                        print('No issues found for given period ... ')
-                    else:
-                        for issue in data['issues']:
-                            date_object = datetime.strptime(f"{issue['fields']['created']}", '%Y-%m-%dT%H:%M:%S.%f%z')
-                            print(f"Key: {issue['key']}, Priority: {issue['fields']['priority']['name']}, Created {date_object.strftime('%m/%d/%y')}, Summary: {issue['fields']['summary']}")
-                else:
-                    self.logger.error(f"Error fetching issue: {response.status_code} - {response.text}")
-                print('-----------------------------------------------------------------------------')
-
-        except Exception as e:
-            self.logger.error("An error occurred in data processing ...")
-            self.logger.error(e)
-
-    def loadQueryData(self):
-        #/ rest / api / 3 / search?jql = filter = {filter_id}
-        filter_id = '10009'
-        try:
-            # Set up the request headers
-            headers = {
-                'Content-Type': 'application/json',
-                'Authorization': f'Basic {base64.b64encode(f"{email}:{api_token}".encode("utf-8")).decode("utf-8")}'
-            }
-            url = f'{jira_url}/rest/api/3/search?jql=filter={filter_id}'
-            response = requests.get(url, headers=headers)
-            if response.status_code == 200:
-                self.logger.info('RESPONSE_CODE_200')
-                obj = response.json()
-                issues = obj['issues']
-                for issue in issues:
-                    self.logger.info(f"Issue key: {issue['key']}")
-            else:
-                self.logger.info(f"Error fetching issue: {response.status_code} - {response.text}")
-        except Exception as e:
-            self.logger.error("An error occurred in data processing ...")
-            self.logger.error(e)
-
-    def loadReportData(self):
-        issue_key = 'ISS-227'
-
-        # Set up the request headers
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Basic {base64.b64encode(f"{email}:{api_token}".encode("utf-8")).decode("utf-8")}'
-        }
-
-        # Send the request
-        #response = requests.get(f'{jira_url}/rest/api/2/issue/{issue_key}', headers=headers)
-
-        url = f'{jira_url}/rest/api/3/issue/{issue_key}'
-        #print(url)
-        response = requests.get(url, headers=headers)
-
-        # Check the response
-        if response.status_code == 200:
-            issue_data = response.json()
-            print(f"Issue key: {issue_data['key']}")
-            print(f"Summary: {issue_data['fields']['summary']}")
-            print(f"Status: {issue_data['fields']['status']['name']}")
-            print(
-                f"Assignee: {issue_data['fields']['assignee']['displayName'] if issue_data['fields']['assignee'] else 'Unassigned'}")
-        else:
-            print(f"Error fetching issue: {response.status_code} - {response.text}")
-
     def main(self, *argv):
         try:
             opts, args = getopt.getopt(argv, "d:hewmabcx", ["emails="])
@@ -269,6 +158,7 @@ class ReportEngine:
             sys.exit(2)
 
         only_key = 0
+        key_count = 0
         for opt, arg in opts:
             if opt == '-h':
                 print('---RUNTIME PARAMETERS---')
@@ -296,34 +186,34 @@ class ReportEngine:
             elif opt in "-m":
                 print('forcing monthly report')
             elif opt in "-a":
-                only_key += 1
+                key_count += 1
+                only_key = 1
             elif opt in "-b":
-                only_key += 1
+                key_count += 1
+                only_key = 2
             elif opt in "-c":
-                only_key += 1
+                key_count += 1
+                only_key = 3
             elif opt == '--emails':
                 # Split the comma-separated email addresses and strip any whitespace
                 self.email_list = [email.strip() for email in arg.split(',')]
 
-
-
-
-        if only_key > 1:
-            print("Exactly one of the argument -a, -b, or -c must be provided.")
+        if key_count > 1:
+            self.logger.info("Exactly one of the argument -a, -b, or -c must be provided.")
             exit(-1)
 
         if only_key == 1:
-            print('ONLY running DAILY report')
+            self.logger.info('ONLY running DAILY report')
             self.run_daily_report = True
             self.run_weekly_report = False
             self.run_monthly_report = False
         elif only_key == 2:
-            print('ONLY running WEEKLY report')
+            self.logger.info('ONLY running WEEKLY report')
             self.run_daily_report = False
             self.run_weekly_report = True
             self.run_monthly_report = False
         elif only_key == 3:
-            print('ONLY running MONTHLY report')
+            self.logger.info('ONLY running MONTHLY report')
             self.run_daily_report = False
             self.run_weekly_report = False
             self.run_monthly_report = True
